84c519e5067abc0f5e7e55c37682f854fb5252ca
Bruno <brunobentocaina@gmail.com>
2024-04-04 13:54:34 -0300

    fix

--- OpenAdm.Application/Services/ItensPedidoService.cs
+++ OpenAdm.Application/Services/ItensPedidoService.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Pkg.Errors;
+﻿using Domain.Pkg.Enum;
+using Domain.Pkg.Errors;
 using Domain.Pkg.Exceptions;
 using OpenAdm.Application.Dtos.ItensPedidos;
 using OpenAdm.Application.Interfaces;
@@ -10,16 +11,28 @@ namespace OpenAdm.Application.Services;
 public class ItensPedidoService : IItensPedidoService
 {
     private readonly IItensPedidoRepository _itensPedidoRepository;
+    private readonly IPedidoRepository _pedidoRepository;
 
-    public ItensPedidoService(IItensPedidoRepository itensPedidoRepository)
+    public ItensPedidoService(
+        IItensPedidoRepository itensPedidoRepository,
+        IPedidoRepository pedidoRepository)
     {
         _itensPedidoRepository = itensPedidoRepository;
+        _pedidoRepository = pedidoRepository;
     }
 
     public async Task EditarQuantidadeDoItemAsync(UpdateQuantidadeItemPedidoDto updateQuantidadeItemPedidoDto)
     {
         var item = await _itensPedidoRepository.GetItemPedidoByIdAsync(updateQuantidadeItemPedidoDto.Id)
             ?? throw new ExceptionApi(CodigoErrors.RegistroNotFound);
+
+        var pedido = await _pedidoRepository.GetPedidoByIdAsync(item.PedidoId)
+            ?? throw new ExceptionApi(CodigoErrors.RegistroNotFound);
+
+        if(pedido.StatusPedido != StatusPedido.Aberto)
+        {
+            throw new ExceptionApi("Este pedido já está entregue!");
+        }
 
         item.EditarQuantidade(updateQuantidadeItemPedidoDto.Quantidade);
         await _itensPedidoRepository.UpdateAsync(item);
