ebb8f5091adf7ac86fba13c597df6d815bbb6ee9
Bruno <brunobentocaina@gmail.com>
2025-02-03 21:05:25 -0300

    feature atualizar senha adm

---
+++ OpenAdm.Application/Dtos/Usuarios/AtualizarSenhaUsuarioAdmDto.cs
@@ -0,0 +1,33 @@
+﻿using OpenAdm.Domain.Exceptions;
+
+namespace OpenAdm.Application.Dtos.Usuarios;
+
+public class AtualizarSenhaUsuarioAdmDto
+{
+    public Guid UsuarioId { get; set; }
+    public string Senha { get; set; } = string.Empty;
+    public string ConfirmarSenha { get; set; } = string.Empty;
+
+    public void Validar()
+    {
+        if (UsuarioId == Guid.Empty)
+        {
+            throw new ExceptionApi("Id do usuário inválido");
+        }
+
+        if (string.IsNullOrWhiteSpace(Senha))
+        {
+            throw new ExceptionApi("Senha inválida");
+        }
+
+        if (string.IsNullOrWhiteSpace(ConfirmarSenha))
+        {
+            throw new ExceptionApi("Confirme a senha");
+        }
+
+        if (!Senha.Equals(ConfirmarSenha))
+        {
+            throw new ExceptionApi("As senha não conferem");
+        }
+    }
+}
---
+++ OpenAdm.Application/Services/AtualizarSenhaUsuarioAdmService.cs
@@ -0,0 +1,30 @@
+﻿using OpenAdm.Application.Adapters;
+using OpenAdm.Application.Dtos.Usuarios;
+using OpenAdm.Application.Interfaces;
+using OpenAdm.Domain.Exceptions;
+using OpenAdm.Domain.Interfaces;
+
+namespace OpenAdm.Application.Services;
+
+public class AtualizarSenhaUsuarioAdmService : IAtualizarSenhaUsuarioAdmService
+{
+    private readonly IUsuarioRepository _usuarioRepository;
+
+    public AtualizarSenhaUsuarioAdmService(IUsuarioRepository usuarioRepository)
+    {
+        _usuarioRepository = usuarioRepository;
+    }
+
+    public async Task<bool> AtualizarAsync(AtualizarSenhaUsuarioAdmDto atualizarSenhaUsuarioAdmDto)
+    {
+        atualizarSenhaUsuarioAdmDto.Validar();
+        var usuario = await _usuarioRepository.GetUsuarioByIdAsync(atualizarSenhaUsuarioAdmDto.UsuarioId)
+            ?? throw new ExceptionApi("Não foi possível localizar o cadastro do usuário");
+
+        usuario.UpdateSenha(PasswordAdapter.GenerateHash(atualizarSenhaUsuarioAdmDto.Senha));
+
+        await _usuarioRepository.UpdateAsync(usuario);
+
+        return true;
+    }
+}
