8c96bfc64c8596dbafe69c6edad2f0c79c567131
bruno <brunobentocaina@gmail.com>
2026-04-19 22:10:28 -0300

    modificado msg notificação novo pedido

--- OpenAdm.Worker.Application/Service/NotificarNovoPedidoService.cs
+++ OpenAdm.Worker.Application/Service/NotificarNovoPedidoService.cs
@@ -75,7 +75,7 @@ public class NotificarNovoPedidoService : IEventoAplicacaoService
                 Mediatype = "document",
                 Mimetype = "application/pdf",
                 Caption =
-                    $"{parceiro.NomeFantasia}\nÓtima noticia, o cliente {pedido.Usuario.Nome} fez um novo pedido \nNúmero do pedido: {pedido.Numero}",
+                    $@"🛒 *Novo pedido confirmado!*\n{parceiro.NomeFantasia}\n👤 Cliente: {pedido.Usuario.Nome}\n🧾 Pedido: #{pedido.Numero}\n💰 Total: {pedido.ValorTotal.FormatMoney()}.",
                 Media = Convert.ToBase64String(pdf),
                 FileName = $"pedido-{pedido.Numero}.pdf",
                 Delay = 0,
