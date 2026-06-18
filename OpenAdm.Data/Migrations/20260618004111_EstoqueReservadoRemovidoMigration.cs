260f18264f0d2eb3491fdb582371ad929bc4db5e
bruno <brunobentocaina@gmail.com>
2026-06-17 21:41:46 -0300

    fix

---
+++ OpenAdm.Data/Migrations/20260618004111_EstoqueReservadoRemovidoMigration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenAdm.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class EstoqueReservadoRemovidoMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "QuantidadeReservada",
+                table: "Estoques");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Estoques_ProdutoId_PesoId_TamanhoId",
+                table: "Estoques",
+                columns: new[] { "ProdutoId", "PesoId", "TamanhoId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Estoques_ProdutoId_PesoId_TamanhoId",
+                table: "Estoques");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "QuantidadeReservada",
+                table: "Estoques",
+                type: "numeric(12,2)",
+                precision: 12,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
