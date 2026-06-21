98cba1710cf2056e6ba817e96c41864c167ce060
bruno <brunobentocaina@gmail.com>
2026-06-20 21:29:05 -0300

    fix

--- OpenAdm.Application/Models/Home/HomeECommerceViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿using OpenAdm.Domain.Model;
-
-namespace OpenAdm.Application.Models.Home;
-
-public class HomeECommerceViewModel
-{
-    public IEnumerable<LojaParceiraModel> LojasParceiras { get; set; } = [];
-}
