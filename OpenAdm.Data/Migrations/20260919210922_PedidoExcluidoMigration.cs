86439307a7d14a7d0d45d99286862a234b908cfc
bruno <brunobentocaina@gmail.com>
2026-09-19 18:10:09 -0300

    fix

---
+++ OpenAdm.Data/Migrations/20260919210922_PedidoExcluidoMigration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenAdm.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class PedidoExcluidoMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Excluido",
+                table: "Pedidos",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pedidos_Excluido_StatusPedido",
+                table: "Pedidos",
+                columns: new[] { "Excluido", "StatusPedido" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Pedidos_Excluido_StatusPedido",
+                table: "Pedidos");
+
+            migrationBuilder.DropColumn(
+                name: "Excluido",
+                table: "Pedidos");
+        }
+    }
+}
